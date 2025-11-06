9a0a2719e1327213757aaf5ac11c62e69b62cd9d
costaalacuparmare <vconstantinescu@protonmail.com>
2025-11-06 09:06:16 +0200

    B done

--- Colociu1_1/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/Colocviu1_1MainActivity.kt
+++ Colociu1_1/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/Colocviu1_1MainActivity.kt
@@ -1,6 +1,9 @@
 package ro.pub.cs.systems.eim.colocviu1_1
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,31 +18,80 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ro.pub.cs.systems.eim.colocviu1_1.ui.theme.Colocviu1_1MainActivityTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
+
+    var pressedButton = mutableStateOf("")
+    var nrPresses = mutableIntStateOf(0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            pressedButton = mutableStateOf(savedInstanceState.getString("butt", ""))
+            nrPresses.intValue = savedInstanceState.getInt("count", 0)
+            Log.d(TAG, "onCreate: restored butt=${pressedButton} count=${nrPresses}")
+        } else {
+            Log.d(TAG, "onCreate: fresh start butt=${pressedButton} count=${nrPresses}")
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Colocviu1_1MainActivityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Colocviu1_UI(
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        onPress = { direction ->
+                            pressedButton.value += "$direction, "
+                            nrPresses.intValue += 1
+                        },
+                        nrPresses = nrPresses.intValue,
+                        pressedButton = pressedButton.value
                     )
                 }
             }
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: butt=${pressedButton} count=${nrPresses}")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("butt", pressedButton.value)
+        outState.putInt("count", nrPresses.intValue)
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState: saved butt=${pressedButton} count=${nrPresses}")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: butt=${pressedButton} count=${nrPresses}")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: butt=${pressedButton} count=${nrPresses}")
+    }
 }
 
 @Composable
-fun Colocviu1_UI(modifier: Modifier = Modifier) {
-    // 4 buttons for each compass direction arranged in star pattern */
+fun Colocviu1_UI(
+    modifier: Modifier = Modifier,
+    onPress: (String) -> Unit = { _ -> },
+    nrPresses: Int = 0,
+    pressedButton: String = ""
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -53,7 +105,9 @@ fun Colocviu1_UI(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             // Buttons would go here
-            Button(onClick = { /* TODO */ }) {
+            Button(onClick = {
+                onPress("NORTH")
+            }) {
                 Text("NORTH")
             }
         }
@@ -68,7 +122,9 @@ fun Colocviu1_UI(modifier: Modifier = Modifier) {
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Button(onClick = { /* TODO */ }) {
+                Button(onClick = {
+                    onPress("WEST")
+                }) {
                     Text("WEST")
                 }
             }
@@ -80,7 +136,9 @@ fun Colocviu1_UI(modifier: Modifier = Modifier) {
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Button(onClick = { /* TODO */ }) {
+                Button(onClick = {
+                    onPress("EAST")
+                }) {
                     Text("EAST")
                 }
             }
@@ -90,10 +148,17 @@ fun Colocviu1_UI(modifier: Modifier = Modifier) {
             modifier = Modifier.padding(16.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Button(onClick = { /* TODO */ }) {
+            Button(onClick = {
+                onPress("SOUTH")
+            }) {
                 Text("SOUTH")
             }
         }
+
+        //unediting space to show what button is pressed based on class var
+        Text("Number of presses: $nrPresses")
+        Text("Pressed buttons: $pressedButton")
+
 
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -108,6 +173,8 @@ fun Colocviu1_UI(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Colocviu1_1MainActivityTheme {
-        Colocviu1_UI()
+        Colocviu1_UI(
+        )
+
     }
 }
