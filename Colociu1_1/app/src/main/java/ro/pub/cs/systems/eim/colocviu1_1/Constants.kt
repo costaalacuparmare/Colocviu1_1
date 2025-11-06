3e81e6a336a7700697f0329ed8c347c36cec64b0
costaalacuparmare <vconstantinescu@protonmail.com>
2025-11-06 10:29:48 +0200

    modified icon and namespace

--- Colociu1_1/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/Constants.kt
+++
@@ -1,9 +0,0 @@
-package ro.pub.cs.systems.eim.colocviu1_1
-
-const val TAG = "Colocviu1_1"
-
-const val RESULT_REGISTER = 1
-
-const val RESULT_CANCELED = 0
-
-const val PRIMARY_ACTION = "PrimaryAction"
